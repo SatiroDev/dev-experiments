d89da1a52ba177223315b2d87f3a79d4a410d554
José Satiro <jardelsatiro57@gmail.com>
2025-10-19 15:53:20 -0300

    adiciona PDF de teste e script para ler PDF

---
+++ read-of-pdf/read_pdf.py
@@ -0,0 +1,13 @@
+import pdfplumber
+
+# caminho onde se encontra o pdf
+caminho = "./hugging_face/test.pdf"
+
+# abre o pdf
+with pdfplumber.open(caminho) as pdf:
+    texto = ""
+    # percorre todas as páginas do pdf
+    for pag in pdf.pages:
+        texto += pag.extract_text() or ""
+
+print(texto)
